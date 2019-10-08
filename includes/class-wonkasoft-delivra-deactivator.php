bf61cab25779ee90fd0a452d85ba2d3184519c55
llister15 <llister@wonkasoft.com>
2019-10-08 15:24:34 -0700

    first commit

---
+++ includes/class-wonkasoft-delivra-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://wonkasoft.com
+ * @since      1.0.0
+ *
+ * @package    Wonkasoft_Delivra
+ * @subpackage Wonkasoft_Delivra/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wonkasoft_Delivra
+ * @subpackage Wonkasoft_Delivra/includes
+ * @author     Wonkasoft, LLC <support@wonkasoft.com>
+ */
+class Wonkasoft_Delivra_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
